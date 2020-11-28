89595991b9af8863374abc1cc38ea43cad53ddf1
mikiyas lemlemu <Mikiyaslemlemu@gmail.com>
2020-11-28 19:03:33 +0300

    Add files via upload

--- includes/script.php
+++ includes/script.php
@@ -1,12 +1,6 @@
 <script src="tools/pkgs/jquery/jquery-3.3.1.min.js"></script>
-<script src="tools/pkgs/particlejs/particles.js"></script>
-<!-- <script src="tools/pkgs/particlejs/js/app.js"></script>
-<script src="tools/pkgs/particlejs/js/lib/stats.js"></script>
-<script src="tools/pkgs/owl/owl.carousel.min.js"></script> -->
 <script src="tools/pkgs/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 <script src="tools/pkgs/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js"></script>
-<script src="tools/pkgs/sweetalert/sweetalert2.all.min.js"></script>
-<script src="tools/pkgs/sweetalert/sweetalert2.min.js"></script>
 <script src="tools/pkgs/snackbar/snackbar.js"></script>
 <script src="tools/pkgs/styled_img/jasny-bootstrap.js"></script>
 <script src="tools/js/main.js"></script>
---
+++ tools/js/main.js
@@ -0,0 +1,45 @@
+function showme(layout){
+
+        Snackbar.show({text: '<i class="fa fa-refresh fa-spin"></i>  Loading <span class="dot1 text-white">.</span><span class="dot2 text-white">.</span><span class="dot3 text-white">.</span>',duration: 2000,pos: 'bottom-right',showAction: false,backgroundColor: '#343a40',textColor: '#fff'});
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                $(".body").html(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET", "layouts/index.php?fol=" + layout, true);
+        xmlhttp.send();
+}
+function verify(value,req){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                $(".body").html(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET", "autoloads/index.php?val=" + value + "&req=" + req , true);
+        xmlhttp.send();
+}
+
+
+
+$(document).ready(function(e){
+      $.ajaxSetup({cache:false});
+      setInterval(function() {$('.headp').load('includes/nav.php')}, 2000);
+});
+// $(document).ready(function(e){
+//       $.ajaxSetup({cache:false});
+//       setInterval(function() {$('#code').load('layouts/code.php','request=' + $('#request_id').val())}, 2000);
+// });
+// $(document).ready(function(e){
+//       $.ajaxSetup({cache:false});
+//       setInterval(function() {$('#phone').load('layouts/phone.php','request=' + $('#request_id').val())}, 2000);
+// });
+// $(document).ready(function(e){
+//       $.ajaxSetup({cache:false});
+//       setInterval(function() {$('#duration').load('layouts/time.php','request=' + $('#request_id').val())}, 2000);
+// });
+$(document).ready(function(e){
+      $.ajaxSetup({cache:false});
+      setInterval(function() {$('#changeme').load('layouts/password.php')}, 2000);
+});
